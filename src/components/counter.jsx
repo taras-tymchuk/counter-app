adbc1a3e8a99783e576b2ae4edc0a438520ab44c
taras-tymchuk <tymakruk@gmail.com>
2019-06-01 01:42:46 +0300

    Added Counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,24 +1,37 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl:
-      "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwipmKCevr7iAhXWwAIHHa-fCCAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fnature%2F&psig=AOvVaw10UgBKySX0_gDg2uj7qFCK&ust=1559141874392390"
+    count: 0
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className='btn btn-secondary btn-sm'
+        >
+          Increment
+        </button>
       </div>
     );
   }
 
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    return count === 0 ? 'Zero' : count;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Counters from './components/counters';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import 'font-awesome/css/font-awesome.css';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Counters />, document.getElementById('root'));
 serviceWorker.unregister();
