fb2fad5fe248cae1d0d6a0c1bfb551b2afa61744
taras-tymchuk <tymakruk@gmail.com>
2019-06-01 14:55:26 +0300

    Added decrement button

--- src/App.js
+++ src/App.js
@@ -13,11 +13,28 @@ class App extends Component {
     ]
   };
 
+  constructor() {
+    super();
+    console.log('App - constructor');
+  }
+
+  componentDidMount() {
+    console.log('App - Mounted');
+  }
+
   handleIncrement = counter => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
     counters[index] = { ...counter };
     counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
     this.setState({ counters });
   };
 
@@ -47,6 +64,7 @@ class App extends Component {
             counters={this.state.counters}
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete}
           />
         </main>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,19 +3,25 @@ import Counter from './counter';
 
 class Counters extends Component {
   render() {
+    const {
+      onDelete,
+      onIncrement,
+      onDecrement,
+      onReset,
+      counters
+    } = this.props;
+
     return (
       <div>
-        <button
-          onClick={this.props.onReset}
-          className='btn btn-primary btn-sm m-2'
-        >
+        <button onClick={onReset} className='btn btn-primary btn-sm m-2'>
           Reset
         </button>
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
             counter={counter}
           />
         ))}
